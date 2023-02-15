8477354502a5837ce028146f6d6cb0cba031dfef
callmeoli <olikorma@gmail.com>
2023-02-15 15:54:26 +0300

    hot_fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,30 @@
 """ index for out api """
 
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status_route():
     """ Status of the web server"""
     return jsonify({"status": 'OK'})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
 
 if __name__ == '__main__':
     pass
